ba3ad1bdfc4adb2a7a586d9348f667e56dd15a2b
Fizza Zaman <44647981+fzaman6505@users.noreply.github.com>
2020-09-06 10:01:44 -0700

    Add files via upload

---
+++ Chess/src/ColorThread.java
@@ -0,0 +1,21 @@
+
+
+/**
+ *
+ * @author fzama
+ */
+import java.io.PrintWriter;
+
+public class ColorThread extends Thread{
+	private ListenerClient listener;
+	private Client client;
+	
+	ColorThread(ListenerClient listen, Client cli){
+		this.listener = listen;
+		this.client = cli;
+	}
+	
+	public void run(){
+		this.client.chooseColor();
+	}
+}
---
+++ Chess/src/Empty.java
@@ -0,0 +1,22 @@
+
+
+/**
+ *
+ * @author fzama
+ */
+
+public class Empty extends Piece{
+		
+		public Empty(int x, int y, Board b){
+			super("none", x, y, b);
+			this.name = ".";
+		}
+		
+		public Boolean isValidMovement(int x, int y){
+			return false;
+		}
+		
+		public Boolean isEmpty(){
+			return true;
+		}
+}
